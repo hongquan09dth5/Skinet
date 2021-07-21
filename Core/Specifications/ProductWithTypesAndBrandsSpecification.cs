ad24b405f67aa074f99fa60dde8ed6300a671376
unknown <lhquan.ktcn@gmail.com>
2021-07-21 22:10:58 +0700

    End of Section 4

---
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,19 @@
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductWithTypesAndBrandsSpecification()
+        {
+            AddInclude(p => p.ProductType);
+            AddInclude(p => p.ProductBrand);
+        }
+
+        public ProductWithTypesAndBrandsSpecification(int id) : base(p => p.Id == id)
+        {
+            AddInclude(p => p.ProductType);
+            AddInclude(p => p.ProductBrand);
+        }
+    }
+}
